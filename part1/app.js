786b8a091e4a4a0a6ae99b730f8575c559976e66
LimitGap <a1911056@adelaide.edu.au>
2025-06-20 08:45:14 +0000

    [automatic commit] 2025-06-20 08:45:13 UTC 49.184.117.61

--- part1/app.js
+++ part1/app.js
@@ -21,11 +21,9 @@ app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 app.get('/api/dogs', async (req, res) => {
   try {
-    const [rows] = await db.execute(`
-      SELECT Dogs.name AS dog_name, Dogs.size AS size, Users.username AS owner_username
-      FROM Dogs
-      JOIN Users ON Dogs.owner_id = Users.user_id
-    `);
+    const [rows] = await db.execute('
+      SELECT Dogs.name AS dog_name, Dogs.size AS size, Users.username AS owner_username FROM Dogs
+      JOIN Users ON Dogs.owner_id = Users.user_id');
     res.json(rows);
   } catch (err) {
     res.status(500).json({ error: 'Database error', details: err.message });

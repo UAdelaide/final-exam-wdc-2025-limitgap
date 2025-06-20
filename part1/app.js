ed573962266e8b4327113496323c7f279f159d22
LimitGap <a1911056@adelaide.edu.au>
2025-06-20 10:10:55 +0000

    [automatic commit] 2025-06-20 10:10:55 UTC 49.184.117.61

--- part1/app.js
+++ part1/app.js
@@ -60,12 +60,12 @@ let db;
     const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
     if (rows[0].count === 0) {
       await db.execute(`
-        INSERT INTO Dogs (owner_id, name, size) VALUES
-        ((SELECT user_id FROM Users WHERE username='alice123'), 'Max', 'medium'),
-        ((SELECT user_id FROM Users WHERE username='carol123'), 'Bella', 'small'),
-        ((SELECT user_id FROM Users WHERE username='alice123'), 'Ben', 'large'),
-        ((SELECT user_id FROM Users WHERE username='carol123'), 'Princess', 'medium'),
-        ((SELECT user_id FROM Users WHERE username='aaron123'), 'Pluto', 'large')
+        INSERT INTO Users (username, email, password_hash, role) VALUES
+        ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+        ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+        ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+        ('aaron123', 'aaron@example.com', 'hashed000', 'owner'),
+        ('john123', 'john@example.com', 'hashed111', 'walker')
       `);
     }
 

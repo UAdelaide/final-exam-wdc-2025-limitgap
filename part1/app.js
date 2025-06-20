d20fc351cf2918e3be600ab879ee94538ed57bef
LimitGap <a1911056@adelaide.edu.au>
2025-06-20 09:34:03 +0000

    [automatic commit] 2025-06-20 09:34:03 UTC 49.184.117.61

--- part1/app.js
+++ part1/app.js
@@ -24,7 +24,7 @@ let db;
     });
 
     // Create the database if it doesn't exist
-    await connection.query('');
+    await connection.query('CREATE DATABASE IF NOT EXISTS DogWalk');
     await connection.end();
 
     // Now connect to the created database

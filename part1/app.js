3e2e57e4d8f7fa747ccbdf6d43dfe46da8e38819
LimitGap <a1911056@adelaide.edu.au>
2025-06-20 10:07:18 +0000

    [automatic commit] 2025-06-20 10:07:18 UTC 49.184.117.61

--- part1/app.js
+++ part1/app.js
@@ -53,7 +53,7 @@ let db;
         name VARCHAR(50) NOT NULL,
         size ENUM('small', 'medium', 'large') NOT NULL,
         FOREIGN KEY (owner_id) REFERENCES Users(user_id))
-        `);
+    `);
 
     // Insert data if table is empty
     const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Dogs');

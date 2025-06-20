2b22afe83fdc45f353963c15b1afcece87fd4a4d
LimitGap <a1911056@adelaide.edu.au>
2025-06-20 09:43:33 +0000

    [automatic commit] 2025-06-20 09:43:32 UTC 49.184.117.61

--- part1/app.js
+++ part1/app.js
@@ -47,10 +47,12 @@ let db;
     const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Dogs');
     if (rows[0].count === 0) {
       await db.execute(`
-        INSERT INTO books (title, author) VALUES
-        ('1984', 'George Orwell'),
-        ('To Kill a Mockingbird', 'Harper Lee'),
-        ('Brave New World', 'Aldous Huxley')
+        INSERT INTO Dogs (owner_id, name, size) VALUES
+((SELECT user_id FROM Users WHERE username='alice123'), 'Max', 'medium'),
+((SELECT user_id FROM Users WHERE username='carol123'), 'Bella', 'small'),
+((SELECT user_id FROM Users WHERE username='alice123'), 'Ben', 'large'),
+((SELECT user_id FROM Users WHERE username='carol123'), 'Princess', 'medium'),
+((SELECT user_id FROM Users WHERE username='aaron123'), 'Pluto', 'large');
       `);
     }
   } catch (err) {

103267b8402d79615ac2faa7bcd2cb9f74ec0e4f
LimitGap <a1911056@adelaide.edu.au>
2025-06-20 09:57:04 +0000

    [automatic commit] 2025-06-20 09:57:03 UTC 49.184.117.61

--- part1/app.js
+++ part1/app.js
@@ -38,7 +38,7 @@ let db;
     await db.execute(`
       CREATE TABLE IF NOT EXISTS Users (
         user_id INT AUTO_INCREMENT PRIMARY KEY,
-        username VARCHAR(50) NOT NULL,
+        username VARCHAR(50) UNIQUE NOT NULL,
         email VARCHAR(100)
       )
     `);

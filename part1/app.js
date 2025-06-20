1d2b947cb1be4170afe0c00f398f5f5ba7a964aa
LimitGap <a1911056@adelaide.edu.au>
2025-06-20 10:12:42 +0000

    [automatic commit] 2025-06-20 10:12:42 UTC 49.184.117.61

--- part1/app.js
+++ part1/app.js
@@ -89,7 +89,7 @@ let db;
 app.get('/api/dogs', async (req, res) => {
   try {
     const [rows] = await db.execute(`
-        SELECT Dogs.name AS `);
+        SELECT Dogs.name AS dog_name, Dogs.size AS size,`);
     res.json(books);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch books' });

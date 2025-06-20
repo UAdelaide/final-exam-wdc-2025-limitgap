d4a2163895798fc651eaecbbbefef82ed61c6684
LimitGap <a1911056@adelaide.edu.au>
2025-06-20 10:00:21 +0000

    [automatic commit] 2025-06-20 10:00:20 UTC 49.184.117.61

--- part1/app.js
+++ part1/app.js
@@ -40,7 +40,8 @@ let db;
         user_id INT AUTO_INCREMENT PRIMARY KEY,
         username VARCHAR(50) UNIQUE NOT NULL,
         email VARCHAR(100) UNIQUE NOT NULL,
-        password_hash VARCHAR(255) 
+        password_hash VARCHAR(255) NOT NULL,
+        role ENUM()
       )
     `);
 

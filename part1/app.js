7b645e929c96d382386cb81ecf6547117823797f
LimitGap <a1911056@adelaide.edu.au>
2025-06-20 09:55:15 +0000

    [automatic commit] 2025-06-20 09:55:14 UTC 49.184.117.61

--- part1/app.js
+++ part1/app.js
@@ -36,7 +36,7 @@ let db;
 
     // Create a table if it doesn't exist
     await db.execute(`
-      CREATE TABLE IF NOT EXISTS Dogs (
+      CREATE TABLE IF NOT EXISTS Users (
         id INT AUTO_INCREMENT PRIMARY KEY,
         title VARCHAR(255),
         author VARCHAR(255)

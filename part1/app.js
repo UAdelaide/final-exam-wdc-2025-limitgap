59ef1bb06772034cd2f637c8cc1d5f576e1ed6e6
LimitGap <a1911056@adelaide.edu.au>
2025-06-20 10:33:29 +0000

    [automatic commit] 2025-06-20 10:33:29 UTC 49.184.126.65

--- part1/app.js
+++ part1/app.js
@@ -80,7 +80,7 @@ app.get('/api/walkrequests/open', async (req, res) => {
   try {
     const [rows5] = await db.execute(`
         SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests.requested_time, WalkRequests.duration_minutes,
-        WalkRequests.location, Users.username as owner_username FROM WalkRequests JOIN Dogs ON WalkRequest.dog_id=Dogs.dog_id
+        WalkRequests.location, Users.username as owner_username FROM WalkRequests JOIN Dogs ON WalkRequests.dog_id=Dogs.dog_id
         JOIN Users ON Dogs.owner_id=Users.user_id WHERE WalkRequests.status='open'
         `);
     res.json(rows5);

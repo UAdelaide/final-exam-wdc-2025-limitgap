dd8204ca1681fd2ba3cafad9be0e8b99af10fdbe
LimitGap <a1911056@adelaide.edu.au>
2025-06-21 05:33:29 +0000

    [automatic commit] 2025-06-21 05:33:29 UTC 129.127.202.112

--- part2/app.js
+++ part2/app.js
@@ -30,4 +30,6 @@ app.post('/api/login', async(req,res)) => {
     if (users.length=0){
         return res.json({success:false, message: "User record does not match in database"})
     }
+    const user = users[0];
+    res.json
 }

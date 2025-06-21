0f55212dee86b581d3e2f6563c37837ca9fd4654
LimitGap <a1911056@adelaide.edu.au>
2025-06-21 05:32:48 +0000

    [automatic commit] 2025-06-21 05:32:48 UTC 129.127.202.112

--- part2/app.js
+++ part2/app.js
@@ -28,6 +28,6 @@ app.post('/api/login', async(req,res)) => {
     con.release();
 
     if (users.length=0){
-        return res.json({success:false, message: "User does"})
+        return res.json({success:false, message: "User record dp"})
     }
 }

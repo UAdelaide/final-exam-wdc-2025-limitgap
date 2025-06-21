6d7750a8c5ce39d5efbdfa28112780d03851eb9c
LimitGap <a1911056@adelaide.edu.au>
2025-06-21 06:11:09 +0000

    [automatic commit] 2025-06-21 06:11:08 UTC 129.127.202.112

--- part2/app.js
+++ part2/app.js
@@ -19,7 +19,7 @@ app.use('/api/users', userRoutes);
 module.exports = app;
 
 const pool=require('./models/db');
-app.post('/api/login', async (req, res)) => {
+app.post('/api/login', async (req, res)); => {
     const{username,password} = req.body;
 
     const con=await pool.getConnection();

67c145bbc0a9953fe62f21e94c5e2cccfdc9ad20
LimitGap <a1911056@adelaide.edu.au>
2025-06-21 05:16:21 +0000

    [automatic commit] 2025-06-21 05:16:21 UTC 129.127.202.112

--- part2/app.js
+++ part2/app.js
@@ -24,5 +24,5 @@ app.post('/api/login', async(req,res)) => {
 
     const con=await pool.getConnection();
 
-    const[users] = await con.query('SELECT * FROM Users WHERE ')
+    const[users] = await con.query('SELECT * FROM Users WHERE email = ? AND')
 }
